1cb53eaad98854eef44026b231731f3fa0e66b90
junior <jgj_junior@yahoo.com.br>
2020-07-29 21:13:52 -0300

    tab icons & badge

--- reactNav/src/Screens/Tabs/TabsScreen.js
+++ reactNav/src/Screens/Tabs/TabsScreen.js
@@ -3,12 +3,47 @@ import HomeScreen from "./HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileScreen from "../ProfileScreen";
 import SettingsScreen from "./SettingsScreen";
+import { Ionicons } from "@expo/vector-icons";
+import HomeIconWithBadge from "./BadgeIcon";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabsScreen() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return (
+              <HomeIconWithBadge
+                name={
+                  focused
+                    ? "ios-information-circle"
+                    : "ios-information-circle-outline"
+                }
+                size={size}
+                color={color}
+              />
+            );
+          }
+          if (route.name === "Settings") {
+            return (
+              <Ionicons
+                name={focused ? "ios-list-box" : "ios-list"}
+                size={size}
+                color={color}
+              />
+            );
+          }
+          if (route.name === "Profile") {
+            return <Ionicons name="md-person" size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "blue",
+        inactiveTintColor: "gray",
+      }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
